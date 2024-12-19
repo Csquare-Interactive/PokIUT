30cc598d1b4d991c239a4f3deb3f75220f249b56
Naihl <bonnelnoah@gmail.com>
2024-12-19 22:28:29 +0100

    feat: TallGrassBase

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,14 +1,71 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
     public float interactionDistance = 3f;
+    private bool isInGrass = false;
+    private float encounterRate = 0.03f;
+    private Vector3 lastPosition;
+    private float distanceThreshold = 1f;
+    private BattleManager battleManager;
+
+
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
             CheckForHealingStation();
+        }
+
+       if (isInGrass && IsMoving())
+        {
+            float distanceMoved = Vector3.Distance(transform.position, lastPosition);
+            if (distanceMoved >= distanceThreshold)
+            {
+                TryStartEncounter();
+                lastPosition = transform.position; 
+            }
+        }
+    }
+
+    private bool IsMoving()
+    {
+        return Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0;
+    }
+
+    private void TryStartEncounter()
+    {
+        if (Random.value < encounterRate)
+        {
+            StartEncounter();
+        }
+    }
+
+    private void StartEncounter()
+    {
+        Debug.Log("Encounter started!");
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Grass"))
+        {
+            isInGrass = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Grass"))
+        {
+            isInGrass = false;
         }
     }
 
