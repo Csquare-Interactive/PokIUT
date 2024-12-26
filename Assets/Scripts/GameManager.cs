2024bd168d970d075e6503f8765078b1dd5213ab
NeekzenTutorials <neekzen.62@gmail.com>
2024-12-26 16:07:24 +0100

    fix: Moves Folders to have a correct Building Project

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -26,4 +26,24 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            Screen.SetResolution(1280, 720, FullScreenMode.Windowed);
+        }
+        else if (Input.GetKeyDown(KeyCode.F2))
+        {
+            Screen.SetResolution(1920, 1080, FullScreenMode.FullScreenWindow);
+        }
+        else if (Input.GetKeyDown(KeyCode.F3))
+        {
+            Screen.SetResolution(2560, 1440, FullScreenMode.FullScreenWindow);
+        }
+        else if (Input.GetKeyDown(KeyCode.F4))
+        {
+            Screen.SetResolution(3840, 2160, FullScreenMode.FullScreenWindow);
+        }
+    }
 }
